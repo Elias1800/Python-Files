4f61fc63ab58f7896059adebfc0833244eb116e5
Elias Barbosa <elias18barbosa@gmail.com>
2025-11-13 22:29:24 -0300

    final

--- recursividade.py
+++ recursividade.py
@@ -11,18 +11,14 @@ def fatorial(n):
 #Sequencia Fibonacci 
 def seq_fibonacci(n):
 
-    seq=[]
- 
-    for  i in range(n+1):
-        seq.append(fibonacci(i))
-    
-    return seq
-
-def fibonacci(n):
-    if n <= 1:
-        return n
+    if n == 1:
+        return [0]
+    elif n == 2:
+        return [0,1]
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        seq_fib = seq_fibonacci(n-1)
+        seq_fib.append(seq_fib[-1] + seq_fib[-2]) #ultimo número + penultimo
+        return seq_fib
     
 if __name__ == '__main__':
     
@@ -73,6 +69,41 @@ if __name__ == '__main__':
                                 break
                             else :
                                 print("Erro: Opção invalida!\n")
+            
+            case 2: 
+
+                while validador:  
+
+                    try:
+                        entrada = int(input("Insira um valor fibonacci: "))
+
+                    except ValueError:
+                        print("Erro: Digite apenas números inteiros.\n")
+
+                    except RecursionError:
+                        print("Erro: Valor fornecido é muito grande.\n")
+                    
+                    if entrada < 0:
+                        print("Erro: número negativo não possui fibonacci.\n")
+                        continue
+
+                    else:
+                        print(seq_fibonacci(entrada),"\n")
+
+                        while True:
+
+                            ft_nv = input("Digite 0 para sair ou 1 para continuar: ")
+
+                            if ft_nv.lower() == '0': 
+                                os.system('cls')
+                                validador = False
+                                break
+                            elif ft_nv.lower() == 'n':
+                                os.system('cls')
+                                break
+                            else :
+                                print("Erro: Opção invalida!\n")
+
             case 0:
                 print("Programa finalizado!")
                 break
